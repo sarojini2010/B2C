83fa8d1b43fc2cde6d8a7b07eca808fc835b2ab2
sarojini2010 <behera.sarojini.2010@gmail.com>
2016-08-02 15:04:58 +0530

    1Aug6.05

--- app/src/main/java/com/example/thoughtchimp/dummyapplication/Constant.java
+++ app/src/main/java/com/example/thoughtchimp/dummyapplication/Constant.java
@@ -10,5 +10,8 @@ public interface Constant
     String SessionDetailIp="http://192.168.0.103/s2m-b2c/Api/session_details?session_id=28&format=json";
     String SessionArchieveIp="http://192.168.0.105/s2m-b2c/Api/archive?child_id=9";
     String ACTIVATIONIP="http://192.168.0.105/s2m-b2c/Api/activation_details";
+    String USER_SESSION_ID = "usersessionid";
+    String SIGNIN="http://192.168.0.103/s2m-b2c/Authapi/auth_email&format=json";
+    String USER_NAME="name";
 
 }
