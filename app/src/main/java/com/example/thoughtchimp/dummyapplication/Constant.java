4ed5802f6a5770e976d4ac739474871804045b16
sarojini2010 <behera.sarojini.2010@gmail.com>
2016-08-03 19:30:54 +0530

    3August17.17

--- app/src/main/java/com/example/thoughtchimp/dummyapplication/Constant.java
+++ app/src/main/java/com/example/thoughtchimp/dummyapplication/Constant.java
@@ -6,7 +6,7 @@ package com.example.thoughtchimp.dummyapplication;
 public interface Constant
 {
 
-    String CHILDHOMEIP="http://192.168.0.103/s2m-b2c/Api/child_home?child_id=10";
+    String CHILDHOMEIP="http://192.168.0.103/s2m-b2c/Api/child_home?child_id=9";
     String SessionDetailIp="http://192.168.0.103/s2m-b2c/Api/session_details?session_id=13";
     String SessionArchieveIp="http://192.168.0.105/s2m-b2c/Api/archive?child_id=9";
     String ACTIVATIONIP="http://192.168.0.105/s2m-b2c/Api/activation_details";
